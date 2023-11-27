4dc4afc47ed97610d66880d41c3a4b5eb50f87c3
Piotr Lenczewski <piotrlenczewskipl@o2.pl>
2023-11-27 21:54:48 +0100

    Implemented creating heuristic map for different board size

--- Player.py
+++ Player.py
@@ -1,19 +1,20 @@
-import numpy as np
-
-
 class Player:
     def __init__(self, number=None):
         self.number = number
 
     def get_move(self, state):
-        raise NotImplementedError("Get_move is implemented by subclasses. Use one of them.")
+        raise NotImplementedError(
+            "Get_move is implemented by subclasses. Use one of them."
+        )
 
 
 class HumanPlayer(Player):
     def get_move(self, state):
         while True:
             try:
-                row, col = [int(x) for x in input("Enter move (row, column): ").split(", ")]
+                row, col = [
+                    int(x) for x in input("Enter move (row, column): ").split(", ")
+                ]
                 move = (row, col)
 
                 if state.is_valid_move(move):
@@ -29,7 +30,6 @@ class ComputerPlayer(Player):
     def __init__(self, max_depth):
         super().__init__()
         self.max_depth = max_depth
-        self.heuristic_map = [[3, 2, 3], [2, 4, 2], [3, 2, 3]]
 
     def get_move(self, state):
         _, move = self.minimax(state, self.number, self.max_depth)
@@ -48,11 +48,13 @@ class ComputerPlayer(Player):
         scores = []
         moves = []
 
-        for i in range(state.rows):
-            for j in range(state.columns):
+        for i in range(state.board_size):
+            for j in range(state.board_size):
                 if state.is_valid_move((i, j)):
                     state.make_move((i, j), player_number)
-                    score, _ = self.minimax(state, self.opponent_number(player_number), depth - 1)
+                    score, _ = self.minimax(
+                        state, self.opponent_number(player_number), depth - 1
+                    )
                     state.board[i][j] = 0
 
                     scores.append(score)
@@ -67,12 +69,12 @@ class ComputerPlayer(Player):
 
     def heuristic(self, state):
         result = 0
-        for i in range(state.rows):
-            for j in range(state.columns):
+        for i in range(state.board_size):
+            for j in range(state.board_size):
                 if state.board[i][j] == self.number:
-                    result += self.heuristic_map[i][j]
+                    result += state.heuristic_map[i][j]
                 elif state.board[i][j] == self.opponent_number(self.number):
-                    result -= self.heuristic_map[i][j]
+                    result -= state.heuristic_map[i][j]
         return result
 
     def opponent_number(self, player_number):
--- State.py
+++ State.py
@@ -7,12 +7,12 @@ Tile = {0: " ", 1: "O", 2: "X"}
 class State:
     def __init__(self, board_size):
         self.board_size = board_size
-        self.rows, self.columns = board_size
-        self.board = np.full((self.rows, self.columns), 0)
+        self.board = np.full((self.board_size, self.board_size), 0)
+        self.heuristic_map = self.make_heuristic_map()
 
     def print_board(self):
-        for i in range(self.rows):
-            for j in range(self.columns):
+        for i in range(self.board_size):
+            for j in range(self.board_size):
                 print(f"|{Tile[self.board[i][j]]}", end="")
             print("|")
         print()
@@ -21,23 +21,35 @@ class State:
         return not any(0 in row for row in self.board)
 
     def is_winner(self, player_number):
-        for i in range(self.rows):
-            if all(self.board[i, j] == player_number for j in range(self.columns)) or all(
-                self.board[j, i] == player_number for j in range(self.columns)
-            ):
+        for i in range(self.board_size):
+            if all(
+                self.board[i][j] == player_number for j in range(self.board_size)
+            ) or all(self.board[j][i] == player_number for j in range(self.board_size)):
                 return True
 
-        if all(self.board[i, i] == player_number for i in range(self.rows)) or all(
-            self.board[i, 2 - i] == player_number for i in range(self.rows)
-        ):
+        if all(
+            self.board[i][i] == player_number for i in range(self.board_size)
+        ) or all(self.board[i][2 - i] == player_number for i in range(self.board_size)):
             return True
 
         return False
 
     def is_valid_move(self, move):
         row, column = move
-        return  0 <= column < self.columns and self.rows > row >= 0 == self.board[row, column]
+        return (
+            0 <= column < self.board_size
+            and self.board_size > row >= 0 == self.board[row][column]
+        )
 
     def make_move(self, move, player_number):
         row, column = move
-        self.board[row, column] = player_number
+        self.board[row][column] = player_number
+
+    def make_heuristic_map(self):
+        heuristic_map = np.full((self.board_size, self.board_size), 2)
+        for i in range(self.board_size):
+            heuristic_map[i][i] = 3
+            heuristic_map[i][self.board_size - 1 - i] = 3
+        if self.board_size % 2 == 1:
+            heuristic_map[self.board_size // 2][self.board_size // 2] = 4
+        return heuristic_map
