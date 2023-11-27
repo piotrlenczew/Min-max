297afa61b10e8e7f55a9fd64bd867e0d48fb307a
Piotr Lenczewski <piotrlenczewskipl@o2.pl>
2023-11-27 21:11:20 +0100

    Working version with minimax computer player

--- Game.py
+++ Game.py
@@ -2,12 +2,12 @@ from State import State
 
 
 class Game:
-    def __init__(self, player1, player2):
+    def __init__(self, player1, player2, board_size):
         player1.number = 1
         player2.number = 2
         self.players = [player1, player2]
         self.current_player = player1
-        self.state = State()
+        self.state = State(board_size)
 
     def switch_player(self):
         self.current_player = (
@@ -21,21 +21,14 @@ class Game:
             self.state.print_board()
             move = self.current_player.get_move(self.state)
 
-            while not self.state.is_valid_move(move):
-                print("Invalid move. Try again.")
-                move = self.current_player.get_move()
+            self.state.make_move(move, self.current_player.number)
 
-            self.state.make_move(move, self.current_player)
-
-            if self.state.is_winner(self.current_player):
+            if self.state.is_winner(self.current_player.number):
                 self.state.print_board()
                 print(f"Player {self.current_player.number} wins!")
                 return
 
             self.switch_player()
 
-        if not self.state.is_winner(self.players[0]) and not self.state.is_winner(
-            self.players[1]
-        ):
-            self.state.print_board()
-            print("It's a tie!")
+        self.state.print_board()
+        print("It's a tie!")
--- State.py
+++ State.py
@@ -5,12 +5,14 @@ Tile = {0: " ", 1: "O", 2: "X"}
 
 
 class State:
-    def __init__(self):
-        self.board = np.zeros((3, 3), dtype=int)
+    def __init__(self, board_size):
+        self.board_size = board_size
+        self.rows, self.columns = board_size
+        self.board = np.full((self.rows, self.columns), 0)
 
     def print_board(self):
-        for i in range(3):
-            for j in range(3):
+        for i in range(self.rows):
+            for j in range(self.columns):
                 print(f"|{Tile[self.board[i][j]]}", end="")
             print("|")
         print()
@@ -18,24 +20,24 @@ class State:
     def is_full(self):
         return not any(0 in row for row in self.board)
 
-    def is_winner(self, player):
-        for i in range(3):
-            if all(self.board[i, j] == player.number for j in range(3)) or all(
-                self.board[j, i] == player.number for j in range(3)
+    def is_winner(self, player_number):
+        for i in range(self.rows):
+            if all(self.board[i, j] == player_number for j in range(self.columns)) or all(
+                self.board[j, i] == player_number for j in range(self.columns)
             ):
                 return True
 
-        if all(self.board[i, i] == player.number for i in range(3)) or all(
-            self.board[i, 2 - i] == player.number for i in range(3)
+        if all(self.board[i, i] == player_number for i in range(self.rows)) or all(
+            self.board[i, 2 - i] == player_number for i in range(self.rows)
         ):
             return True
 
         return False
 
     def is_valid_move(self, move):
-        row, col = move
-        return 0 <= row < 3 and 0 <= col < 3 and self.board[row, col] == 0
+        row, column = move
+        return  0 <= column < self.columns and self.rows > row >= 0 == self.board[row, column]
 
-    def make_move(self, move, player):
-        row, col = move
-        self.board[row, col] = player.number
+    def make_move(self, move, player_number):
+        row, column = move
+        self.board[row, column] = player_number
